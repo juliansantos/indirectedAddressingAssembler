7881eb00b1b6bbbba81369eb2b04b1921c7ae7df
JulianSB <julian.9621@gmail.com>
2016-07-31 12:35:38 -0500

    It was add PREINCn Command

--- main.asm
+++ main.asm
@@ -1,9 +1,8 @@
-;//rcall -> Para que sirve  
-;//DAW -> Para que sirve
 ;    // que es instruction overhead
     #include <P18F2550.INC>
     list p=PIC18F2550
     
+   
     #define sa1l 0x60
     #define sa1h 0x00
     #define sad 0x0060 
@@ -13,17 +12,22 @@
     ENDC
     
     org 0x0
+    
 main:
+     
  movlw 0x05
- movwf sa1 
+ movwf sa1	    ;sa1=5
  movlw 0x04
- movwf sa2
+ movwf sa2	    ;sa2=4
  movlw 0x03
- movwf sa3 
+ movwf sa3	    ;sa3=3
+ 
  lfsr 1,sad   ;Load in FSR1 sad [0060h]
  movlw 0x89
- movwf INDF1  ;Load in FSR* 89h 	
- movlw 0x00
+ movwf INDF1  ;Load in FSR* 89h => sa1=89	
+ movlw 0x21
+ movwf PREINC1 ;fsr1++ => sa2=21
+ 
  movf sa1,W
  movf sa2,W
  movf sa3,W
